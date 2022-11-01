d9fb2132a24c51beb52c08e9a0d3470a62b3d947
Fabio Teichmann <ft@blocksize-capital.com>
2022-11-01 19:08:23 +0200

    variables

--- src/lectures/demo/variables/variables.go
+++ src/lectures/demo/variables/variables.go
@@ -3,4 +3,34 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Fabio"
+	fmt.Println("my name is", myName)
+
+	var name string = "Kathy"
+	fmt.Println("name =", name)
+
+	userName := "admin"
+	fmt.Println("username =", userName)
+
+	var sum int
+	fmt.Println("the sum is", sum)
+
+	part1, other := 1, 5
+	fmt.Println("part1 is", part1, "; other is", other)
+
+	part2, other := 1, 0
+	fmt.Println("part1 is", part2, "; other is", other)
+
+	sum = part1 + part2
+	fmt.Println("sum =", sum)
+
+	var (
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+	fmt.Println("lesson name:", lessonName)
+	fmt.Println("lesson type:", lessonType)
+
+	word1, word2, _ := "hello", "world", "!"
+	fmt.Println(word1, word2)
 }
--- src/lectures/go.mod
+++ src/lectures/go.mod
@@ -1,3 +1,8 @@
 module coursecontent
 
 go 1.17
+
+
+//require (
+//    github.com/fatih/color v1.13.0
+//)
