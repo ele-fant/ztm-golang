6dc96a9d2136644808be30656b8b7bd6f56b6aa7
Fabio Teichmann <ft@blocksize-capital.com>
2022-11-02 15:47:26 +0200

    functions

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
+func double(x int) int {
+	return x + x
+}
 
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func greet() {
+	fmt.Println("Hello from Greet-Function!")
+}
+
+func main() {
+	greet()
+
+	dozen := double(6)
+	fmt.Println("a dozen is", dozen)
+
+	bakersDozen := add(dozen, 1)
+	fmt.Println("a baker's dozen is", bakersDozen)
+
+	anotherBakersDozen := add(double(6), 1)
+	fmt.Println("another baker's dozen:", anotherBakersDozen)
 }
--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -19,6 +19,53 @@ package main
 
 import "fmt"
 
-func main() {
+// --Requirements:
+//   - Write a function that accepts a person's name as a function
+//     parameter and displays a greeting to that person.
+func greetPerson(name string) {
+	fmt.Println("Hello,", name, "!")
+}
 
+//   - Write a function that returns any message, and call it from within
+//     fmt.Println()
+func returnAnyMessage(message string) string {
+	return message
+}
+
+//   - Write a function to add 3 numbers together, supplied as arguments, and
+//     return the answer
+func addThreeNumbers(num1, num2, num3 int) int {
+	return num1 + num2 + num3
+}
+
+// * Write a function that returns any number
+func returnAnyNumber() int {
+	return 4
+}
+
+// * Write a function that returns any two numbers
+func returnAnyTwoNums() (int, int) {
+	return 1, 6
+}
+
+func main() {
+	greetPerson("Fabio")
+
+	fmt.Println(returnAnyMessage("Random message..."))
+
+	sumOfTriple := addThreeNumbers(1, 2, 3)
+	fmt.Println("Sum of triple is:", sumOfTriple)
+
+	anyNumber := returnAnyNumber()
+	fmt.Println("Random number:", anyNumber)
+
+	a, b := returnAnyTwoNums()
+	fmt.Println(a, b)
+
+	//* Add three numbers together using any combination of the existing functions.
+	//  * Print the result
+	anotherThree := addThreeNumbers(returnAnyNumber(), a, b)
+	fmt.Println(anotherThree)
+
+	//* Call every function at least once
 }
