f18eb12863e481a82166ada3cd5f0065766ad130
Fabio Teichmann <ft@blocksize-capital.com>
2022-11-07 17:56:42 +0200

    structs

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -17,8 +17,72 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// * Create a rectangle structure containing its coordinates
+type Rectangle struct {
+	A Point
+	B Point
+}
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func width(r Rectangle) float64 {
+	return math.Abs(r.B.X - r.A.X)
+}
+
+func length(r Rectangle) float64 {
+	return math.Abs(r.B.Y - r.A.Y)
+}
+
+func lineLength(a Point, b Point) float64 {
+	l := math.Sqrt(math.Pow((b.Y-a.Y), 2) + math.Pow((b.X-a.X), 2))
+	return l
+}
+
+func area(r Rectangle) float64 {
+	//p1 := Point{
+	//	X: r.A.X,
+	//	Y: r.B.Y,
+	//}
+	//a, b := lineLength(r.A, p1), lineLength(r.B, p1)
+	return width(r) * length(r)
+}
+
+func perimiter(r Rectangle) float64 {
+	//p1 := Point{
+	//	X: r.A.X,
+	//	Y: r.B.Y,
+	//}
+	//a, b := lineLength(r.A, p1), lineLength(r.B, p1)
+	return width(r) + length(r)
+}
 
 func main() {
+	p1, p2 := Point{X: 1, Y: 1}, Point{X: 5, Y: 4}
+	rect := Rectangle{A: p1, B: p2}
+	fmt.Println(rect)
 
+	a := area(rect)
+	fmt.Println("Area is:", a)
+
+	perim := perimiter(rect)
+	fmt.Println("Perimiter is:", perim)
+
+	rect.B.X *= 2
+	rect.A.X *= 2
+
+	fmt.Println("With doubled parameter:")
+	fmt.Println(rect)
+	a = area(rect)
+	fmt.Println("Area is:", a)
+
+	perim = perimiter(rect)
+	fmt.Println("Perimiter is:", perim)
 }
